3e3947847a76f5732b417b83f6f6ffc83d7650e7
shivabasayy <86300624+shivabasayy@users.noreply.github.com>
2021-06-30 10:27:20 +0530

    Update Calculator.java
    Extra coded is added

--- src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
+++ src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
@@ -59,6 +59,8 @@ public class Calculator {
 		System.out.println("The sum of two numbers is: "+cc.addFucn(first, second));
 		System.out.println("The difference of two numbers is: "+cc.subFucn(first, second));
 		System.out.println("The multiples of two numbers is: "+cc.mulFucn(first, second));
+		System.out.println("This code is changed to devops student );
+		
 		
 	}
 	 
